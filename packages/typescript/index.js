4fa12bc2fa9821e29f93dc6769979fbbcec82f30
zhangshize <zhangshize@kuaishou.com>
2023-01-12 14:37:23 +0800

    feat: 完善 typescript eslint配置

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -4,5 +4,22 @@ module.exports = {
     'plugin:@typescript-eslint/recommended'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint']
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // 覆盖基础配置
+    'no-unused-vars': 0,
+    'no-undef': 0,
+    'no-useless-constructor': 0,
+    // 覆盖 typescript-eslint 推荐配置
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }
+    ],
+    '@typescript-eslint/comma-dangle': ['error', 'never'],
+    '@typescript-eslint/ban-ts-comment': ['error', 'allow-with-description']
+  }
 }
