1d651029d4ca3c861dfc024fdba3e72f7ecb8efd
zhangshize <zhangshize@kuaishou.com>
2023-01-12 12:02:52 +0800

    feat: Initial eslint-config

---
+++ packages/typescript/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    '@fearcleari/eslint-config-basic',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint']
+}
