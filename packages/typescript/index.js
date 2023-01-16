be389641ee6573e153e770caa2a7f824efffb463
zhangshize <zhangshize@kuaishou.com>
2023-01-16 16:05:48 +0800

    feat: add release-please-config

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -10,7 +10,7 @@ module.exports = {
     'no-unused-vars': 0,
     'no-undef': 0,
     'no-useless-constructor': 0,
-    // 覆盖 typescript-eslint 推荐配置
+    // 覆盖 typescript-eslint 配置
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
