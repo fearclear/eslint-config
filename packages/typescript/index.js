76f20a8b8b4aeaff570f0ec0b9460b68413a9839
zhangshize <zhangshize@kuaishou.com>
2023-03-02 11:42:45 +0800

    feat: update vue implements

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: [
-    '@fearcleari/eslint-config-basic',
-    'plugin:@typescript-eslint/recommended'
-  ],
+  extends: ['@fearcleari/eslint-config-basic', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   rules: {
